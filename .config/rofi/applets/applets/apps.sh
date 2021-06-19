1655cd988f6d680a4b0d77573b5a445895621685
Aditya Gupta <divyashgupta2005@gmail.com>
2021-06-19 19:59:04 +0530

    Switched to Rofi from Dmenu

---
+++ .config/rofi/applets/applets/apps.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+style="$($HOME/.config/rofi/applets/applets/style.sh)"
+
+dir="$HOME/.config/rofi/applets/applets/configs/$style"
+rofi_command="rofi -theme $dir/apps.rasi"
+
+# Links
+terminal=""
+files=""
+editor=""
+browser=""
+music=""
+settings=""
+
+# Error msg
+msg() {
+	rofi -theme "$HOME/.config/rofi/applets/styles/message.rasi" -e "$1"
+}
+
+# Variable passed to rofi
+options="$terminal\n$files\n$editor\n$browser\n$music\n$settings"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Most Used" -dmenu -selected-row 0)"
+case $chosen in
+    $terminal)
+		if [[ -f /usr/bin/termite ]]; then
+			termite &
+		elif [[ -f /usr/bin/urxvt ]]; then
+			urxvt &
+		elif [[ -f /usr/bin/kitty ]]; then
+			kitty &
+		elif [[ -f /usr/bin/xterm ]]; then
+			xterm &
+		elif [[ -f /usr/bin/xfce4-terminal ]]; then
+			xfce4-terminal &
+		elif [[ -f /usr/bin/gnome-terminal ]]; then
+			gnome-terminal &
+		else
+			msg "No suitable terminal found!"
+		fi
+        ;;
+    $files)
+		if [[ -f /usr/bin/nautilus ]]; then
+			nautilus &
+		elif [[ -f /usr/bin/pcmanfm ]]; then
+			pcmanfm &
+		else
+			msg "No suitable file manager found!"
+		fi
+        ;;
+    $editor)
+		if [[ -f /usr/bin/geany ]]; then
+			geany &
+		elif [[ -f /usr/bin/leafpad ]]; then
+			leafpad &
+		elif [[ -f /usr/bin/mousepad ]]; then
+			mousepad &
+		elif [[ -f /usr/bin/code ]]; then
+			code &
+		else
+			msg "No suitable text editor found!"
+		fi
+        ;;
+    $browser)
+		if [[ -f /usr/bin/microsoft-edge ]]; then
+			microsoft-edge &
+		elif [[ -f /usr/bin/micr ]]; then
+			chromium &
+		elif [[ -f /usr/bin/midori ]]; then
+			midori &
+		else
+			msg "No suitable web browser found!"
+		fi
+        ;;
+    $music)
+		flatpak run com.spotify.Client
+        ;;
+    $settings)
+		if [[ -f /usr/bin/xfce4-settings-manager ]]; then
+			xfce4-settings-manager &
+		else
+			msg "No suitable settings manager found!"
+		fi
+        ;;
+esac
